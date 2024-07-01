9c7f9179d4caf76e43bbfa5db9e1e021c4c17e5e
Jake Howard <jake.howard@torchbox.com>
2024-07-01 12:44:35 +0100

    Add dynamic table of contents

--- ons_alpha/core/blocks/markup.py
+++ ons_alpha/core/blocks/markup.py
@@ -1,6 +1,10 @@
+from django.utils.text import slugify
 from wagtail import blocks
 
 
 class HeadingBlock(blocks.CharBlock):
     class Meta:
         template = "templates/components/streamfield/heading_block.html"
+
+    def to_table_of_contents_items(self, value):
+        return [{"url": "#" + slugify(value), "text": value}]
