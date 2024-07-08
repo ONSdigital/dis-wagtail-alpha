3775a8c2872f0c25a0f7b39e0bb3f723d9438ec5
zerolab <dan@zerolab.org>
2024-07-08 16:15:25 +0100

    Only allow bulletin pages under topic pages
    for now

--- ons_alpha/topics/models.py
+++ ons_alpha/topics/models.py
@@ -5,6 +5,7 @@ from ons_alpha.core.models.mixins import SubpageMixin
 class TopicPage(SubpageMixin, BasePage):
     template = "templates/pages/topics/topic_page.html"
     parent_page_types = ["topics.TopicSectionPage"]
+    subpage_types = ["bulletins.BulletinPage"]
     page_description = "A specific topic page. e.g.  Public sector finance or  Inflation and price indices"
 
 
