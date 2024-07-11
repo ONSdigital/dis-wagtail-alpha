b9667097a5b274e7beb6e42d451c81f70772d0d7
Dan Braghiș <31622+zerolab@users.noreply.github.com>
2024-07-11 11:58:15 +0100

    Add support for cell merging and td class (#17)
    * Add support for cell merging and td class
      Note: https://service-manual.ons.gov.uk/design-system/components/table doesn't support col/row spans
    * Override the DS table macro to allow col/row spans

--- ons_alpha/core/blocks/markup.py
+++ ons_alpha/core/blocks/markup.py
@@ -1,5 +1,7 @@
+from django.utils.safestring import mark_safe
 from django.utils.text import slugify
 from wagtail import blocks
+from wagtail.contrib.table_block.blocks import DEFAULT_TABLE_OPTIONS
 from wagtail.contrib.table_block.blocks import TableBlock as WagtailTableBlock
 from wagtail.contrib.typed_table_block.blocks import (
     TypedTableBlock as WagtailTypedTableBlock,
@@ -32,23 +34,96 @@ class TableBlock(WagtailTableBlock):
         icon = "info-circle"
         template = "templates/components/streamfield/table_block.html"
 
-    def get_context(self, value, parent_context=None):
-        context = super().get_context(value, parent_context)  #
+    def __init__(self, required=True, help_text=None, table_options=None, **kwargs):
+        if table_options is None:
+            table_options = {
+                "mergeCells": True,
+                "contextMenu": DEFAULT_TABLE_OPTIONS["contextMenu"] + ["---------", "mergeCells", "alignment"],
+            }
 
-        table_header = (
-            [{"value": cell} for cell in value["data"][0]]
-            if value.get("data", None) and len(value["data"]) > 0 and value.get("first_row_is_table_header", False)
-            else []
-        )
+        super().__init__(required=required, help_text=help_text, table_options=table_options, **kwargs)
+
+    def _to_ons_classname(self, classname: str) -> str:
+        """
+        Reference: https://handsontable.com/docs/javascript-data-grid/text-alignment/#horizontal-and-vertical-alignment
+        """
+        match classname:
+            case "htRight":
+                return "ons-u-ta-right"
+            case "htLeft":
+                return "ons-u-ta-left"
+            case "htCenter":
+                return "ons-u-ta-center"
+            case _:
+                return ""
+
+    def _get_header(
+        self, value, hidden: dict[tuple[int, int], bool], spans: dict[tuple[int, int], str]
+    ) -> list[dict[str, str]]:
+        table_header = []
+        if value.get("data", "") and len(value["data"]) > 0 and value.get("first_row_is_table_header", False):
+            for th_idx, cell in enumerate(value["data"][0]):
+                key = (0, th_idx)
+                if hidden.get(key):
+                    continue
+
+                th = {"value": cell or ""}
+                if span := spans.get(key):
+                    th["span"] = span
+                table_header.append(th)
+        return table_header
+
+    def _get_rows(self, value, classnames, hidden, spans):  # pylint: disable=too-many-locals
+        trs = []
+        has_header = value.get("data", "") and len(value["data"]) > 0 and value.get("first_row_is_table_header", False)
+        data = value["data"][1:] if has_header else value.get("data", [])
+        for row_idx, row in enumerate(data, 1 if has_header else 0):
+            tds = []
+            for cell_idx, cell in enumerate(row):
+                cell_key = (row_idx, cell_idx)
+                if hidden.get(cell_key):
+                    continue
+
+                td = {"value": cell}
+                if classname := classnames.get(cell_key):
+                    td["tdClasses"] = classname
+                if span := spans.get(cell_key):
+                    td["span"] = span
+
+                tds.append(td)
+
+            trs.append({"tds": tds})
+        return trs
+
+    def get_context(self, value, parent_context=None):
+        context = super().get_context(value, parent_context=parent_context)
+
+        classnames = {}
+        hidden = {}
+        spans = {}
+        if value.get("cell"):
+            for meta in value["cell"]:
+                if "className" in meta:
+                    classnames[(meta["row"], meta["col"])] = self._to_ons_classname(meta["className"])
+                if "hidden" in meta:
+                    hidden[(meta["row"], meta["col"])] = meta["hidden"]
+
+        if value.get("mergeCells"):
+            for merge in value["mergeCells"]:
+                span = ""
+                if merge["rowspan"] > 1:
+                    span += f'rowspan="{merge["rowspan"]}" '
+                if merge["colspan"] > 1:
+                    span += f'colspan="{merge["colspan"]}" '
+
+                if span:
+                    spans[(merge["row"], merge["col"])] = mark_safe(span)  # noqa: S308
 
         return {
             "options": {
                 "caption": value.get("table_caption"),
-                "ths": table_header,
-                "trs": [
-                    {"tds": [{"value": cell} for cell in row]}
-                    for row in (value["data"][1:] if table_header else value.get("data", []))
-                ],
+                "ths": self._get_header(value, hidden, spans),
+                "trs": self._get_rows(value, classnames, hidden, spans),
             },
             **context,
         }
