a6223cd0d3935071ddcad64b6d9c75d26dbd25e3
zerolab <dan@zerolab.org>
2024-07-19 14:11:43 +0100

    Limit the bundle chooser to active (ie. non-approved, non-released) bundles

--- ons_alpha/bundles/viewsets.py
+++ ons_alpha/bundles/viewsets.py
@@ -2,9 +2,11 @@ from functools import cached_property
 
 from wagtail.admin.ui.tables import Column, UpdatedAtColumn, UserColumn
 from wagtail.admin.views.generic import CreateView, IndexView
+from wagtail.admin.views.generic.chooser import ChooseView
+from wagtail.admin.viewsets.chooser import ChooserViewSet
 from wagtail.admin.viewsets.model import ModelViewSet
 
-from .models import Bundle
+from .models import Bundle, BundleStatus
 
 
 class BundleCrateView(CreateView):
@@ -16,10 +18,16 @@ class BundleCrateView(CreateView):
 
 
 class BundleIndexView(IndexView):
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        return queryset.select_related("topic", "created_by")
+
     @cached_property
     def columns(self):
         return [
             self._get_title_column("__str__"),
+            Column("topic"),
             Column("scheduled_publication_date"),
             Column("get_status_display", label="Status"),
             UpdatedAtColumn(),
@@ -27,14 +35,43 @@ class BundleIndexView(IndexView):
         ]
 
 
+class BundleChooseView(ChooseView):
+    icon = "boxes-stacked"
+
+    @property
+    def columns(self):
+        return super().columns + [
+            Column("topic", label="Topic", accessor="topic.title"),
+            UserColumn("created_by"),
+        ]
+
+    def get_object_list(self):
+        return Bundle.objects.select_related("topic", "created_by").only("name", "created_by", "topic__title")
+
+
+class BundleChooserViewSet(ChooserViewSet):
+    model = Bundle
+    icon = "boxes-stacked"
+    choose_view_class = BundleChooseView
+    preserve_url_parameters = ["multiple", "topic"]
+    url_filter_parameters = ["topic"]
+
+    def get_object_list(self):
+        return self.model.objects.exclude(status_in=[BundleStatus.APPROVED, BundleStatus.RELEASED])
+
+
 class BundleViewSet(ModelViewSet):
     model = Bundle
     icon = "boxes-stacked"
     add_view_class = BundleCrateView
     index_view_class = BundleIndexView
+    chooser_viewset_class = BundleChooserViewSet
     list_filter = ["status", "created_by"]
     add_to_admin_menu = True
     inspect_view_enabled = True
 
 
 bundle_viewset = BundleViewSet("bundle")
+bundle_chooser_viewset = BundleChooserViewSet("bundle_chooser")
+
+BundleChooserWidget = bundle_chooser_viewset.widget_class
--- ons_alpha/bundles/wagtail_hooks.py
+++ ons_alpha/bundles/wagtail_hooks.py
@@ -1,8 +1,8 @@
 from wagtail import hooks
 
-from .viewsets import bundle_viewset
+from .viewsets import bundle_chooser_viewset, bundle_viewset
 
 
 @hooks.register("register_admin_viewset")
 def register_viewset():
-    return bundle_viewset
+    return [bundle_viewset, bundle_chooser_viewset]
