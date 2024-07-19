0d5fe90d0ba69b6a818fdaec598c7e2793501251
ISLAS AHMED NAWAZ <islas.nawaz@ons.gov.uk>
2024-07-19 11:14:51 +0100

    Fixing linting issues

--- ons_alpha/bulletins/forms.py
+++ ons_alpha/bulletins/forms.py
@@ -1,16 +1,16 @@
 from django import forms
+from django.apps import apps
 from wagtail.admin.forms import WagtailAdminPageForm
+
 
 class BulletinPageAdminForm(WagtailAdminPageForm):
     previous_version = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     class Meta:
-        model = BulletinPage
+        model = apps.get_model("bulletins", "BulletinPage")  # Lazy reference to avoid circular dependency
         fields = "__all__"
 
     def clean(self):
-        from .models import BulletinPage  # Lazy import to avoid circular dependency
-
         cleaned_data = super().clean()
 
         # Remove duplicate topics
