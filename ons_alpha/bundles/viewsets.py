990b7de2b02bfe94d582babba2f4da981712c59c
zerolab <dan@zerolab.org>
2024-07-24 12:53:02 +0100

    Preven editing released bundles

--- ons_alpha/bundles/viewsets.py
+++ ons_alpha/bundles/viewsets.py
@@ -1,20 +1,30 @@
 from functools import cached_property
 
+from django.shortcuts import redirect
 from wagtail.admin.ui.tables import Column, UpdatedAtColumn, UserColumn
-from wagtail.admin.views.generic import CreateView, IndexView
+from wagtail.admin.views.generic import CreateView, EditView, IndexView
 from wagtail.admin.views.generic.chooser import ChooseView
 from wagtail.admin.viewsets.chooser import ChooserViewSet
 from wagtail.admin.viewsets.model import ModelViewSet
 
-from .models import Bundle
+from .models import Bundle, BundleStatus
 
 
 class BundleCreateView(CreateView):
     def save_instance(self):
+        # automatically set the creator
         instance = super().save_instance()
         instance.created_by = self.request.user
         instance.save(update_fields=["created_by"])
         return instance
+
+
+class BundleEditView(EditView):
+    def dispatch(self, request, *args, **kwargs):
+        if (instance := self.get_object()) and instance.status == BundleStatus.RELEASED:
+            return redirect(self.index_url_name)
+
+        return super().dispatch(request, *args, **kwargs)
 
 
 class BundleIndexView(IndexView):
@@ -22,6 +32,15 @@ class BundleIndexView(IndexView):
         queryset = super().get_queryset()
 
         return queryset.select_related("topic", "created_by")
+
+    def get_edit_url(self, instance):
+        # disable the edit URL for released bundles
+        if instance.status != BundleStatus.RELEASED:
+            return super().get_edit_url(instance)
+
+    def get_copy_url(self, instance):
+        # disable copy
+        return
 
     @cached_property
     def columns(self):
@@ -65,6 +84,7 @@ class BundleViewSet(ModelViewSet):
     model = Bundle
     icon = "boxes-stacked"
     add_view_class = BundleCreateView
+    edit_view_class = BundleEditView
     index_view_class = BundleIndexView
     chooser_viewset_class = BundleChooserViewSet
     list_filter = ["status", "created_by"]
