bc2bcb84d181be4ed18f98a38a4df3a6a354deab
AWitcherONS <ann.witcher@ons.gov.uk>
2024-08-01 09:34:31 +0100

    init commit

--- ons_alpha/topics/models.py
+++ ons_alpha/topics/models.py
@@ -1,9 +1,13 @@
 from django.core.exceptions import ValidationError
 from django.db import models
-from wagtail.admin.panels import FieldPanel, ObjectList, TabbedInterface
+from wagtail.admin.panels import (
+    FieldPanel,
+    ObjectList,
+    TabbedInterface)
 
 from ons_alpha.core.models.base import BasePage
 from ons_alpha.core.models.mixins import SubpageMixin
+from ons_alpha.standardpages import blocks
 from ons_alpha.taxonomy.models import Topic
 
 
@@ -41,6 +45,24 @@ class TopicPage(BaseTopicPage):
     parent_page_types = ["topics.TopicSectionPage"]
     subpage_types = ["bulletins.BulletinSeriesPage"]
     page_description = "A specific topic page. e.g. Public sector finance or Inflation and price indices"
+    summary = models.CharField(blank=True, max_length=255)
+
+
+    content_panels = BasePage.content_panels + [
+        FieldPanel("summary"),
+    ]
+
+    edit_handler = TabbedInterface(
+        [
+            ObjectList(content_panels, heading="Content"),
+            ObjectList(
+                [FieldPanel("topic")],
+                help_text="Select the topics that this page relates to.",
+                heading="Taxonomy",
+            ),
+            ObjectList(BasePage.promote_panels, heading="Promote"),
+        ]
+    )
 
     def clean(self):
         super().clean()
@@ -52,6 +74,23 @@ class TopicSectionPage(BaseTopicPage):
     template = "templates/pages/topics/topic_section_page.html"
     subpage_types = ["topics.TopicSectionPage", "topics.TopicPage"]
     page_description = "General topic page. e.g. Economy"
+    summary = models.CharField(blank=True, max_length=255)
+
+    content_panels = BasePage.content_panels + [
+            FieldPanel("summary"),
+        ]
+
+    edit_handler = TabbedInterface(
+        [
+            ObjectList(content_panels, heading="Content"),
+            ObjectList(
+                [FieldPanel("topic")],
+                help_text="Select the topics that this page relates to.",
+                heading="Taxonomy",
+            ),
+            ObjectList(BasePage.promote_panels, heading="Promote"),
+        ]
+    )
 
     def clean(self):
         super().clean()
