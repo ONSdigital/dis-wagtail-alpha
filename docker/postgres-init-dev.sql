8e1f0b5b9c4fafa693f4e425a44a73f130b2035a
Jake Howard <jake.howard@torchbox.com>
2024-08-15 17:03:46 +0100

    Give write access to renditions and form submissions

--- docker/postgres-init-dev.sql
+++ docker/postgres-init-dev.sql
@@ -1,3 +1,4 @@
 -- Create user for testing read-only connections (DATABASE_URL=postgres://ons_alpha_read_only@db/ons_alpha)
 CREATE ROLE ons_alpha_read_only WITH LOGIN;
 GRANT SELECT ON ALL TABLES IN SCHEMA public to ons_alpha_read_only;
+GRANT INSERT, UPDATE ON wagtailimages_rendition, wagtailforms_formsubmission TO ons_alpha_read_only;
