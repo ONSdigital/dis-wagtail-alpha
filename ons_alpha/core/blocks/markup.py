16e6da0f1da43a694ea956a75ddbb059be572c0c
ISLAS AHMED NAWAZ <islas.nawaz@ons.gov.uk>
2024-09-02 22:38:22 +0100

    Refactored code to reduce the number of local variables in accordance with pylint standards.

--- ons_alpha/core/blocks/markup.py
+++ ons_alpha/core/blocks/markup.py
@@ -1,10 +1,10 @@
+from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.utils.text import slugify
 from wagtail import blocks
 from wagtail.contrib.table_block.blocks import DEFAULT_TABLE_OPTIONS
 from wagtail.contrib.table_block.blocks import TableBlock as WagtailTableBlock
 from wagtail.contrib.typed_table_block.blocks import TypedTableBlock as WagtailTypedTableBlock
-
 
 class HeadingBlock(blocks.CharBlock):
     class Meta:
@@ -26,7 +26,6 @@ class HeadingBlock(blocks.CharBlock):
 
     def to_table_of_contents_items(self, value):
         return [{"url": "#" + slugify(value), "text": value}]
-
 
 class TableBlock(WagtailTableBlock):
     class Meta:
@@ -52,9 +51,7 @@ class TableBlock(WagtailTableBlock):
             case _:
                 return ""
 
-    def _get_header(
-        self, value, hidden: dict[tuple[int, int], bool], spans: dict[tuple[int, int], str]
-    ) -> list[dict[str, str]]:
+    def _get_header(self, value, hidden: dict[tuple[int, int], bool], spans: dict[tuple[int, int], str]) -> list[dict[str, str]]:
         table_header = []
         if value.get("data", "") and len(value["data"]) > 0 and value.get("first_row_is_table_header", False):
             for th_idx, cell in enumerate(value["data"][0]):
@@ -71,20 +68,26 @@ class TableBlock(WagtailTableBlock):
         trs = []
         has_header = value.get("data", "") and len(value["data"]) > 0 and value.get("first_row_is_table_header", False)
         data = value["data"][1:] if has_header else value.get("data", [])
+        
         for row_idx, row in enumerate(data, 1 if has_header else 0):
-            tds = []
-            for cell_idx, cell in enumerate(row):
-                cell_key = (row_idx, cell_idx)
-                if hidden.get(cell_key):
-                    continue
-                td = {"value": cell}
-                if classname := classnames.get(cell_key):
-                    td["tdClasses"] = classname
-                if span := spans.get(cell_key):
-                    td["span"] = span
-                tds.append(td)
+            tds = self._process_row(row, row_idx, classnames, hidden, spans)
             trs.append({"tds": tds})
+        
         return trs
+
+    def _process_row(self, row, row_idx, classnames, hidden, spans):
+        tds = []
+        for cell_idx, cell in enumerate(row):
+            cell_key = (row_idx, cell_idx)
+            if hidden.get(cell_key):
+                continue
+            td = {"value": cell}
+            if classname := classnames.get(cell_key):
+                td["tdClasses"] = classname
+            if span := spans.get(cell_key):
+                td["span"] = span
+            tds.append(td)
+        return tds
 
     def clean(self, value):
         if not value or not value.get("table_header_choice"):
@@ -114,7 +117,7 @@ class TableBlock(WagtailTableBlock):
                 if merge["colspan"] > 1:
                     span += f'colspan="{merge["colspan"]}" '
                 if span:
-                    spans[(merge["row"], merge["col"])] = span  # Avoid using mark_safe directly
+                    spans[(merge["row"], merge["col"])] = span
         return {
             "options": {
                 "caption": value.get("table_caption"),
@@ -126,7 +129,6 @@ class TableBlock(WagtailTableBlock):
 
     def render(self, value, context=None):
         return super(blocks.FieldBlock, self).render(value, context)
-
 
 class TypedTableBlock(blocks.StructBlock):
     caption = blocks.CharBlock(required=False)
@@ -154,7 +156,6 @@ class TypedTableBlock(blocks.StructBlock):
     class Meta:
         icon = "table"
         template = "templates/components/streamfield/typed_table_block.html"
-
 
 class QuoteBlock(blocks.StructBlock):
     quote = blocks.CharBlock(form_classname="title")
