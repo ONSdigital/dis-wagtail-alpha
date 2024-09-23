8fcaf6eb4a741cbc63ad53569e9a969214e7beac
Helen C <helen.chapman@torchbox.com>
2024-09-23 13:51:14 +0100

    give option to run docker with front-end or not

--- docker/bashrc.sh
+++ docker/bashrc.sh
@@ -6,5 +6,12 @@ if [ "$BUILD_ENV" = "dev" ]; then
     alias djrun="python manage.py runserver 0.0.0.0:8000"
     alias djrunplus="python manage.py runserver_plus 0.0.0.0:8000"
     alias djtest="python manage.py test --settings=ons_alpha.settings.test"
-    alias honcho="honcho -f docker/Procfile"
 fi
+
+# nvm
+if [ -a "$HOME/.nvm/nvm.sh" ]; then
+  export NVM_DIR="$HOME/.nvm"
+  source "$NVM_DIR/nvm.sh"
+fi
+
+alias honcho="honcho -f docker/Procfile"
