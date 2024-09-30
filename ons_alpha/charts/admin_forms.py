8e4117274e32ec31b3d4f97493ed0d57215d28ee
Andy Babic <andy.babic@torchbox.com>
2024-09-30 12:30:20 +0100

    Tweak fields and implement rendering-related methods

--- ons_alpha/charts/admin_forms.py
+++ ons_alpha/charts/admin_forms.py
@@ -1,24 +1,10 @@
 from django import forms
-from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
-from wagtail.admin.forms import WagtailAdminModelForm
 
-from ons_alpha.charts.constants import DataSource
 from ons_alpha.charts.utils import get_chart_type_choices
 
 
 class ChartTypeSelectForm(forms.Form):
     chart_type = forms.ChoiceField(
-        choices=get_chart_type_choices, required=True
+        choices=get_chart_type_choices, label=_("What type of chart do you want to create?"), required=True
     )
-
-class BaseChartEditForm(WagtailAdminModelForm):
-    data_source = forms.ChoiceField(label=_("Data source"), choices=DataSource.choices, initial=DataSource.CSV)
-
-    def clean(self):
-        cleaned_data = super().clean()
-        if cleaned_data["data_source"] == DataSource.CSV and not cleaned_data["data_file"]:
-            raise ValidationError({"data_file": _("This field is required")})
-        if cleaned_data["data_source"] == DataSource.MANUAL and not cleaned_data["data"]:
-            raise ValidationError({"data_file": _("This field is required")})
-        return cleaned_data
