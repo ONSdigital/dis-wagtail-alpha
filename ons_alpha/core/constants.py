a75efd67652e2e0443855e4476d5b4a90ea41dcb
Andy Babic <andy.babic@torchbox.com>
2024-10-02 17:37:00 +0100

    Add CONTENT_TYPE_LABEL_CHOICES

---
+++ ons_alpha/core/constants.py
@@ -0,0 +1,7 @@
+CONTENT_TYPE_LABEL_CHOICES = (
+    ("Analysis", "Analysis"),
+    ("Article", "Article"),
+    ("Dataset", "Dataset"),
+    ("Methodology", "Methodology"),
+    ("Time series", "Time series"),
+)
