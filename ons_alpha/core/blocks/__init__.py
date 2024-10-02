cec2c2517f0e099e6c2a7d33707cfef66c984547
Andy Babic <andy.babic@torchbox.com>
2024-10-02 17:42:44 +0100

    Add DocumentListBlock to CoreStoryBlock

--- ons_alpha/core/blocks/__init__.py
+++ ons_alpha/core/blocks/__init__.py
@@ -1,4 +1,6 @@
+from .document_list import DocumentListBlock
 from .embeddable import DocumentBlock, DocumentsBlock, ImageBlock, ONSEmbedBlock
+from .featured_document import FeaturedDocumentBlock
 from .markup import HeadingBlock, ONSTableBlock, QuoteBlock, TableBlock, TypedTableBlock
 from .panels import CorrectionBlock, NoticeBlock, PanelBlock
 from .related import RelatedContentBlock, RelatedLinksBlock
@@ -8,6 +10,8 @@ from .snippets import ChartChooserBlock
 __all__ = [
     "CorrectionBlock",
     "DocumentBlock",
+    "DocumentListBlock",
+    "FeaturedDocumentBlock",
     "HeadingBlock",
     "ImageBlock",
     "NoticeBlock",
