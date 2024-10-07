0db42a4065c2240311665926e30b9d8f994825a9
Andy Babic <andy.babic@torchbox.com>
2024-10-07 17:41:04 +0100

    Fix CSV file handling

--- ons_alpha/charts/validators.py
+++ ons_alpha/charts/validators.py
@@ -10,7 +10,7 @@ BLANK_VALUES = ("", "NULL", "-", "---")
 def csv_file_validator(file):
     # check file valid csv format
     try:
-        csv.Sniffer().sniff(file.read(1024))
+        csv.Sniffer().sniff(file.read(1024).decode("utf-8"))
         file.seek(0)
     except csv.Error as e:
         raise ValidationError(_("The file is not a valid CSV.")) from e
