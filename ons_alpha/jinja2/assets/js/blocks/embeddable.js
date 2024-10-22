12b799897de8fb6a1c306f4e23fa509372892e9a
Helen Chapman <helen.chapman@torchbox.com>
2024-10-22 17:28:10 +0100

    Further work on iframe resize issue (#111)
    * Further possible fix for the iframe resize
    
    * Load pymjs from cdn, as recommoneded in the docs, and move the js embeddable code to a class
    
    * Reinstate local pym.js due to security risk

--- ons_alpha/jinja2/assets/js/blocks/embeddable.js
+++
@@ -1,8 +0,0 @@
-// required to set height of pym iframes used in ONSEmbeddable block
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('div.pym-interactive').forEach(function (element) {
-        return new pym.Parent(element.getAttribute('id'), element.dataset.url, {
-            title: element.dataset.title
-        });
-    });
-});
