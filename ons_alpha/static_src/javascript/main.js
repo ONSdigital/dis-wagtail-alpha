07ba0edab2cbc3a5fb37a2e1830b9535d772cdeb
Helen C <helen.chapman@torchbox.com>
2024-10-22 14:13:16 +0100

    Load pymjs from cdn, as recommoneded in the docs, and move the js embeddable code to a class

--- ons_alpha/static_src/javascript/main.js
+++ ons_alpha/static_src/javascript/main.js
@@ -1,1 +1,12 @@
+import IframeResize from './components/iframe-resize';
+
 import '../sass/main.scss';
+
+function initComponent(ComponentClass) {
+    const items = document.querySelectorAll(ComponentClass.selector());
+    items.forEach((item) => new ComponentClass(item));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    initComponent(IframeResize);
+});
