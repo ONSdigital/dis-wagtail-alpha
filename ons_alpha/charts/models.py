c2c26f386742a62f562dc0169eb3d4b5f409a594
Andy Babic <andy.babic@torchbox.com>
2024-11-04 17:44:48 +0000

    Fix headers cached property erroring when reading the CSV file

--- ons_alpha/charts/models.py
+++ ons_alpha/charts/models.py
@@ -256,10 +256,11 @@ class BaseHighchartsChart(Chart):
     @cached_property
     def headers(self) -> Sequence[str]:
         if self.data_source == DataSource.CSV and self.data_file:
-            with self.data_file.open("r") as csvfile:
-                textwrapper = io.TextIOWrapper(csvfile, newline="")
+            with self.data_file.open("rb") as csvfile:
+                textwrapper = io.TextIOWrapper(csvfile, encoding="utf-8", newline="")
                 textwrapper.seek(0)
                 reader = csv.reader(textwrapper)
+                next(reader)
                 return reader.fieldnames or []
         if self.data_source == DataSource.MANUAL and self.data_manual:
             return [col["heading"] for col in self.manual_data_table.columns]
