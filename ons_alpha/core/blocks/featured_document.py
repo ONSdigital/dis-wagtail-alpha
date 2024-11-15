00724c5ff38125672990ec468f35970b627ad3b1
Andy Babic <andy.babic@torchbox.com>
2024-11-15 12:06:16 +0000

    Update FeaturedDocumentWithChartBlock to use document details already prepared by the parent

--- ons_alpha/core/blocks/featured_document.py
+++ ons_alpha/core/blocks/featured_document.py
@@ -84,12 +84,5 @@ class FeaturedDocumentWithChartBlock(FeaturedDocumentBlock):
 
     def get_context(self, value, parent_context=None):
         context = super().get_context(value, parent_context=parent_context)
-        page = value["page"].specific
-        context["link_title"] = page.headline or page.display_title
-        context["link_url"] = page.get_url(parent_context.get("request") if parent_context else None)
-        if release_date := getattr(page, "release_date", None):
-            context["release_date"] = {
-                "iso": release_date.isoformat(),
-                "short": release_date.strftime("%-d %B %Y"),
-            }
+        context["document"] = context["documents"][0]
         return context
