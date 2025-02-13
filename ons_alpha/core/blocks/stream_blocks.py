dcd6684f624aa44c3187571cc04042dd7b443f79
Nick Smith <nick.smith@torchbox.com>
2025-02-13 12:48:32 +0000

    Move Explore More block to TopicPage

--- ons_alpha/core/blocks/stream_blocks.py
+++ ons_alpha/core/blocks/stream_blocks.py
@@ -8,7 +8,6 @@ from ons_alpha.core.blocks import (
     CorrectionBlock,
     DocumentListBlock,
     DocumentsBlock,
-    ExploreMoreBlock,
     HeadingBlock,
     NoticeBlock,
     ONSChartEmbedBlock,
@@ -50,7 +49,6 @@ class CoreStoryBlock(StreamBlock):
         "Use the 'ONS Table' block for the full ONS needs.",
     )
     document_list = DocumentListBlock()
-    explore_more = ExploreMoreBlock()
 
     class Meta:
         block_counts = {"related_links": {"max_num": 1}}
