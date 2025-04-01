0f1f6158c0772199f4348875bbe9bf34eaa9203f
Maciej Baron <MaciekBaron@users.noreply.github.com>
2025-04-01 10:30:35 +0100

    Add image caption to headline figures block

--- ons_alpha/core/blocks/headline_figures.py
+++ ons_alpha/core/blocks/headline_figures.py
@@ -9,6 +9,7 @@ class HeadlineFiguresItemBlock(StructBlock):
     figure = CharBlock(label=_("Figure"), max_length=10, required=True)
     supporting_text = CharBlock(label=_("Supporting text"), max_length=100, required=False)
     image = ImageChooserBlock(label=_("Image"), required=False)
+    image_caption = CharBlock(label=_("Image caption"), max_length=100, required=False)
 
 
 class HeadlineFiguresBlock(ListBlock):
